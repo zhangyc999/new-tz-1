97731d103259875fcb858d5b70d0a87cf1e5f70e
chuangix <chuangix@qq.com>
2018-09-21 16:51:59 +0800

    Update can.c tz.c

--- app/can.c
+++ app/can.c
@@ -61,12 +61,12 @@ extern MSG_Q_ID msg_sdfb;
 extern MSG_Q_ID msg_mom;
 extern MSG_Q_ID msg_gen;
 
+const extern ECU sys_ecu[255];
+
 static void isr0(void);
 static void isr1(void);
 static void init0(void);
 static void init1(void);
-
-ECU* id2ecu(u8 id);
 
 void t_can(int period)
 {
@@ -122,7 +122,7 @@ static void isr0(void)
         buf.id[1] = READ_REG_BYTE(ADDR(0), PELI_RXB(3));
         buf.id[0] = READ_REG_BYTE(ADDR(0), PELI_RXB(4));
         *(u32 *)&buf.id[0] >>= 3;
-        msg = id2ecu(buf.id[0])->msg;
+        msg = sys_ecu(buf.id[0])->msg;
         if (!msg || buf.id[1] != CA_MAIN) {
                 WRITE_REG_BYTE(ADDR(0), PELI_CMR, 0x04);
                 return;
@@ -155,7 +155,7 @@ static void isr1(void)
         buf.id[1] = READ_REG_BYTE(ADDR(1), PELI_RXB(3));
         buf.id[0] = READ_REG_BYTE(ADDR(1), PELI_RXB(4));
         *(u32 *)&buf.id[0] >>= 3;
-        msg = id2ecu(buf.id[0])->msg;
+        msg = sys_ecu(buf.id[0])->msg;
         if (!msg || buf.id[1] != CA_MAIN) {
                 WRITE_REG_BYTE(ADDR(1), PELI_CMR, 0x04);
                 return;
@@ -219,108 +219,6 @@ static void init1(void)
         WRITE_REG_BYTE(ADDR(1), PELI_MODE, 0x08);
         intConnect(INUM_TO_IVEC(sysInumTbl[7]), (VOIDFUNCPTR)isr1, 0);
         sysIntEnablePIC(7);
-}
-
-ECU* id2ecu(u8 id)
-{
-        const static ECU ecu[255];
-        ecu[CA_TLS0].index = 1;
-        ecu[CA_TLS1].index = 2;
-        ecu[CA_VSL0].index = 3;
-        ecu[CA_VSL1].index = 4;
-        ecu[CA_PSU].index = 5;
-        ecu[CA_SWH0].index = 6;
-        ecu[CA_SWH1].index = 7;
-        ecu[CA_SWH2].index = 8;
-        ecu[CA_SWH3].index = 9;
-        ecu[CA_RSE0].index = 10;
-        ecu[CA_RSE1].index = 11;
-        ecu[CA_RSE2].index = 12;
-        ecu[CA_RSE3].index = 13;
-        ecu[CA_SWV0].index = 14;
-        ecu[CA_SWV1].index = 15;
-        ecu[CA_SWV2].index = 16;
-        ecu[CA_SWV3].index = 17;
-        ecu[CA_PRP0].index = 18;
-        ecu[CA_PRP1].index = 19;
-        ecu[CA_PRP2].index = 20;
-        ecu[CA_PRP3].index = 21;
-        ecu[CA_X0].index = 22;
-        ecu[CA_X1].index = 23;
-        ecu[CA_Y0].index = 24;
-        ecu[CA_Y1].index = 25;
-        ecu[CA_Y2].index = 26;
-        ecu[CA_Y3].index = 27;
-        ecu[CA_Z0].index = 28;
-        ecu[CA_Z1].index = 29;
-        ecu[CA_SDT].index = 30;
-        ecu[CA_SDS0].index = 31;
-        ecu[CA_SDS1].index = 32;
-        ecu[CA_SDS2].index = 33;
-        ecu[CA_SDS3].index = 34;
-        ecu[CA_SDF0].index = 35;
-        ecu[CA_SDF1].index = 36;
-        ecu[CA_SDF2].index = 37;
-        ecu[CA_SDF3].index = 38;
-        ecu[CA_SDB0].index = 39;
-        ecu[CA_SDB1].index = 40;
-        ecu[CA_SDB2].index = 41;
-        ecu[CA_SDB3].index = 42;
-        ecu[CA_MOM0].index = 43;
-        ecu[CA_MOM1].index = 44;
-        ecu[CA_MOM2].index = 45;
-        ecu[CA_MOM3].index = 46;
-        ecu[CA_GEN0].index = 47;
-        ecu[CA_GEN1].index = 48;
-        ecu[CA_TLS0].msg = msg_tls;
-        ecu[CA_TLS1].msg = msg_tls;
-        ecu[CA_VSL0].msg = msg_vsl;
-        ecu[CA_VSL1].msg = msg_vsl;
-        ecu[CA_PSU].msg = msg_psu;
-        ecu[CA_SWH0].msg = msg_swh;
-        ecu[CA_SWH1].msg = msg_swh;
-        ecu[CA_SWH2].msg = msg_swh;
-        ecu[CA_SWH3].msg = msg_swh;
-        ecu[CA_RSE0].msg = msg_rse;
-        ecu[CA_RSE1].msg = msg_rse;
-        ecu[CA_RSE2].msg = msg_rse;
-        ecu[CA_RSE3].msg = msg_rse;
-        ecu[CA_SWV0].msg = msg_swv;
-        ecu[CA_SWV1].msg = msg_swv;
-        ecu[CA_SWV2].msg = msg_swv;
-        ecu[CA_SWV3].msg = msg_swv;
-        ecu[CA_PRP0].msg = msg_prp;
-        ecu[CA_PRP1].msg = msg_prp;
-        ecu[CA_PRP2].msg = msg_prp;
-        ecu[CA_PRP3].msg = msg_prp;
-        ecu[CA_X0].msg = msg_xyz;
-        ecu[CA_X1].msg = msg_xyz;
-        ecu[CA_Y0].msg = msg_xyz;
-        ecu[CA_Y1].msg = msg_xyz;
-        ecu[CA_Y2].msg = msg_xyz;
-        ecu[CA_Y3].msg = msg_xyz;
-        ecu[CA_Z0].msg = msg_xyz;
-        ecu[CA_Z1].msg = msg_xyz;
-        ecu[CA_SDT].msg = msg_sdt;
-        ecu[CA_SDS0].msg = msg_sdt;
-        ecu[CA_SDS1].msg = msg_sds;
-        ecu[CA_SDS2].msg = msg_sds;
-        ecu[CA_SDS3].msg = msg_sds;
-        ecu[CA_SDF0].msg = msg_sds;
-        ecu[CA_SDF1].msg = msg_sdfb;
-        ecu[CA_SDF2].msg = msg_sdfb;
-        ecu[CA_SDF3].msg = msg_sdfb;
-        ecu[CA_SDB0].msg = msg_sdfb;
-        ecu[CA_SDB1].msg = msg_sdfb;
-        ecu[CA_SDB2].msg = msg_sdfb;
-        ecu[CA_SDB3].msg = msg_sdfb;
-        ecu[CA_MOM0].msg = msg_mom;
-        ecu[CA_MOM1].msg = msg_mom;
-        ecu[CA_MOM2].msg = msg_mom;
-        ecu[CA_MOM3].msg = msg_mom;
-        ecu[CA_GEN0].msg = msg_gen;
-        ecu[CA_GEN1].msg = msg_gen;
-        return &ecu[id];
 }
 
 /*
--- app/tz.c
+++ app/tz.c
@@ -1,4 +1,5 @@
 #include "can.h"
+#include "canaddr.h"
 #include "type.h"
 #include "vx.h"
 
@@ -49,9 +50,12 @@ MSG_Q_ID msg_mom;
 MSG_Q_ID msg_gen;
 MSG_Q_ID msg_dbg;
 
+const ECU sys_ecu[255];
+
+static void ecu_init(void)
+
 void tz(void)
 {
-        lstLibInit();
         msg_core = msgQCreate(128, 8, MSG_Q_FIFO);
         msg_can[0][0] = msgQCreate(128, sizeof(CAN), MSG_Q_FIFO);
         msg_can[0][1] = msgQCreate(128, sizeof(CAN), MSG_Q_FIFO);
@@ -74,6 +78,8 @@ void tz(void)
         msg_mom = msgQCreate(64, sizeof(CAN), MSG_Q_FIFO);
         msg_gen = msgQCreate(64, sizeof(CAN), MSG_Q_FIFO);
         msg_dbg = msgQCreate(128, sizeof(CAN), MSG_Q_FIFO);
+        lstLibInit();
+        ecu_init();
         sysClkRateSet(100);
         tid_core = taskSpawn("CORE", 40, VX_FP_TASK, 20000, (FUNCPTR)t_core, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         tid_can = taskSpawn("CAN", 40, VX_FP_TASK, 20000, (FUNCPTR)t_can, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
@@ -89,4 +95,104 @@ void tz(void)
         tid_mom = taskSpawn("MOM", 90, VX_FP_TASK, 20000, (FUNCPTR)t_mom, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         tid_dbg = taskSpawn("DBG", 100, VX_FP_TASK, 20000, (FUNCPTR)t_dbg, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         udp_server();
+}
+
+static void ecu_init(void)
+{
+        sys_ecu[CA_TLS0].index = 1;
+        sys_ecu[CA_TLS1].index = 2;
+        sys_ecu[CA_VSL0].index = 3;
+        sys_ecu[CA_VSL1].index = 4;
+        sys_ecu[CA_PSU].index = 5;
+        sys_ecu[CA_SWH0].index = 6;
+        sys_ecu[CA_SWH1].index = 7;
+        sys_ecu[CA_SWH2].index = 8;
+        sys_ecu[CA_SWH3].index = 9;
+        sys_ecu[CA_RSE0].index = 10;
+        sys_ecu[CA_RSE1].index = 11;
+        sys_ecu[CA_RSE2].index = 12;
+        sys_ecu[CA_RSE3].index = 13;
+        sys_ecu[CA_SWV0].index = 14;
+        sys_ecu[CA_SWV1].index = 15;
+        sys_ecu[CA_SWV2].index = 16;
+        sys_ecu[CA_SWV3].index = 17;
+        sys_ecu[CA_PRP0].index = 18;
+        sys_ecu[CA_PRP1].index = 19;
+        sys_ecu[CA_PRP2].index = 20;
+        sys_ecu[CA_PRP3].index = 21;
+        sys_ecu[CA_X0].index = 22;
+        sys_ecu[CA_X1].index = 23;
+        sys_ecu[CA_Y0].index = 24;
+        sys_ecu[CA_Y1].index = 25;
+        sys_ecu[CA_Y2].index = 26;
+        sys_ecu[CA_Y3].index = 27;
+        sys_ecu[CA_Z0].index = 28;
+        sys_ecu[CA_Z1].index = 29;
+        sys_ecu[CA_SDT].index = 30;
+        sys_ecu[CA_SDS0].index = 31;
+        sys_ecu[CA_SDS1].index = 32;
+        sys_ecu[CA_SDS2].index = 33;
+        sys_ecu[CA_SDS3].index = 34;
+        sys_ecu[CA_SDF0].index = 35;
+        sys_ecu[CA_SDF1].index = 36;
+        sys_ecu[CA_SDF2].index = 37;
+        sys_ecu[CA_SDF3].index = 38;
+        sys_ecu[CA_SDB0].index = 39;
+        sys_ecu[CA_SDB1].index = 40;
+        sys_ecu[CA_SDB2].index = 41;
+        sys_ecu[CA_SDB3].index = 42;
+        sys_ecu[CA_MOM0].index = 43;
+        sys_ecu[CA_MOM1].index = 44;
+        sys_ecu[CA_MOM2].index = 45;
+        sys_ecu[CA_MOM3].index = 46;
+        sys_ecu[CA_GEN0].index = 47;
+        sys_ecu[CA_GEN1].index = 48;
+        sys_ecu[CA_TLS0].msg = msg_tls;
+        sys_ecu[CA_TLS1].msg = msg_tls;
+        sys_ecu[CA_VSL0].msg = msg_vsl;
+        sys_ecu[CA_VSL1].msg = msg_vsl;
+        sys_ecu[CA_PSU].msg = msg_psu;
+        sys_ecu[CA_SWH0].msg = msg_swh;
+        sys_ecu[CA_SWH1].msg = msg_swh;
+        sys_ecu[CA_SWH2].msg = msg_swh;
+        sys_ecu[CA_SWH3].msg = msg_swh;
+        sys_ecu[CA_RSE0].msg = msg_rse;
+        sys_ecu[CA_RSE1].msg = msg_rse;
+        sys_ecu[CA_RSE2].msg = msg_rse;
+        sys_ecu[CA_RSE3].msg = msg_rse;
+        sys_ecu[CA_SWV0].msg = msg_swv;
+        sys_ecu[CA_SWV1].msg = msg_swv;
+        sys_ecu[CA_SWV2].msg = msg_swv;
+        sys_ecu[CA_SWV3].msg = msg_swv;
+        sys_ecu[CA_PRP0].msg = msg_prp;
+        sys_ecu[CA_PRP1].msg = msg_prp;
+        sys_ecu[CA_PRP2].msg = msg_prp;
+        sys_ecu[CA_PRP3].msg = msg_prp;
+        sys_ecu[CA_X0].msg = msg_xyz;
+        sys_ecu[CA_X1].msg = msg_xyz;
+        sys_ecu[CA_Y0].msg = msg_xyz;
+        sys_ecu[CA_Y1].msg = msg_xyz;
+        sys_ecu[CA_Y2].msg = msg_xyz;
+        sys_ecu[CA_Y3].msg = msg_xyz;
+        sys_ecu[CA_Z0].msg = msg_xyz;
+        sys_ecu[CA_Z1].msg = msg_xyz;
+        sys_ecu[CA_SDT].msg = msg_sdt;
+        sys_ecu[CA_SDS0].msg = msg_sdt;
+        sys_ecu[CA_SDS1].msg = msg_sds;
+        sys_ecu[CA_SDS2].msg = msg_sds;
+        sys_ecu[CA_SDS3].msg = msg_sds;
+        sys_ecu[CA_SDF0].msg = msg_sds;
+        sys_ecu[CA_SDF1].msg = msg_sdfb;
+        sys_ecu[CA_SDF2].msg = msg_sdfb;
+        sys_ecu[CA_SDF3].msg = msg_sdfb;
+        sys_ecu[CA_SDB0].msg = msg_sdfb;
+        sys_ecu[CA_SDB1].msg = msg_sdfb;
+        sys_ecu[CA_SDB2].msg = msg_sdfb;
+        sys_ecu[CA_SDB3].msg = msg_sdfb;
+        sys_ecu[CA_MOM0].msg = msg_mom;
+        sys_ecu[CA_MOM1].msg = msg_mom;
+        sys_ecu[CA_MOM2].msg = msg_mom;
+        sys_ecu[CA_MOM3].msg = msg_mom;
+        sys_ecu[CA_GEN0].msg = msg_gen;
+        sys_ecu[CA_GEN1].msg = msg_gen;
 }
